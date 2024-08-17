95118dbe2278296a4342780434b0c1280a567646
tjazerzen <tjaz.erzen@zerodays.dev>
2024-08-17 16:32:10 +0200

    feat: update oil config

--- init.lua
+++ init.lua
@@ -765,8 +765,38 @@ require("lazy").setup({
 		config = function()
 			local oil = require("oil")
 			oil.setup({
+				use_default_keymaps = false,
 				view_options = {
 					show_hidden = true,
+				},
+				-- I'm not using default keymaps, because I want to discard some (commented out)
+				-- I ignore <C-s> and <C-h> because there interfere with my window management in vim.
+				-- I ignore <C-t> and <C-p> because there interfere with zellij config
+				keymaps = {
+					["g?"] = "actions.show_help",
+					["<CR>"] = "actions.select",
+					-- ["<C-s>"] = {
+					-- 	"actions.select",
+					-- 	opts = { vertical = true },
+					-- 	desc = "Open the entry in a vertical split",
+					-- },
+					-- ["<C-h>"] = {
+					-- 	"actions.select",
+					-- 	opts = { horizontal = true },
+					-- 	desc = "Open the entry in a horizontal split",
+					-- },
+					-- ["<C-t>"] = { "actions.select", opts = { tab = true }, desc = "Open the entry in new tab" },
+					-- ["<C-p>"] = "actions.preview",
+					-- ["<C-c>"] = "actions.close",
+					["<C-l>"] = "actions.refresh",
+					["-"] = "actions.parent",
+					["_"] = "actions.open_cwd",
+					["`"] = "actions.cd",
+					["~"] = { "actions.cd", opts = { scope = "tab" }, desc = ":tcd to the current oil directory" },
+					["gs"] = "actions.change_sort",
+					["gx"] = "actions.open_external",
+					["g."] = "actions.toggle_hidden",
+					-- ["g\\"] = "actions.toggle_trash",
 				},
 			})
 
