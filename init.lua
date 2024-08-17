196ada2555afe5c617401fa461c52dc6f6ef355d
tjazerzen <tjaz.erzen@zerodays.dev>
2024-08-17 15:15:32 +0200

    feat: update init.lua

--- init.lua
+++ init.lua
@@ -98,7 +98,7 @@ vim.opt.foldlevelstart = 99
 vim.opt.foldenable = true
 
 -- place a column line
-vim.opt.colorcolumn = "80"
+vim.opt.colorcolumn = "120"
 
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
@@ -191,20 +191,28 @@ require("lazy").setup({
 		config = function() -- This is the function that runs, AFTER loading
 			require("which-key").setup()
 
-			-- Document existing key chains
-			require("which-key").register({
-				["<leader>c"] = { name = "[C]ode", _ = "which_key_ignore" },
-				["<leader>d"] = { name = "[D]ocument", _ = "which_key_ignore" },
-				["<leader>r"] = { name = "[R]ename", _ = "which_key_ignore" },
-				["<leader>s"] = { name = "[S]earch", _ = "which_key_ignore" },
-				["<leader>w"] = { name = "[W]orkspace", _ = "which_key_ignore" },
-				["<leader>t"] = { name = "[T]oggle", _ = "which_key_ignore" },
-				["<leader>h"] = { name = "Git [H]unk", _ = "which_key_ignore" },
+			local wk = require("which-key")
+
+			wk.add({
+				{ "<leader>c", group = "[C]ode" },
+				{ "<leader>c_", hidden = true },
+				{ "<leader>d", group = "[D]ocument" },
+				{ "<leader>d_", hidden = true },
+				{ "<leader>r", group = "[R]ename" },
+				{ "<leader>r_", hidden = true },
+				{ "<leader>s", group = "[S]earch" },
+				{ "<leader>s_", hidden = true },
+				{ "<leader>w", group = "[W]orkspace" },
+				{ "<leader>w_", hidden = true },
+				{ "<leader>t", group = "[T]oggle" },
+				{ "<leader>t_", hidden = true },
+				{ "<leader>h", group = "Git [H]unk" },
+				{ "<leader>h_", hidden = true },
+				{
+					mode = "v",
+					{ "<leader>h", desc = "Git [H]unk" },
+				},
 			})
-			-- visual mode
-			require("which-key").register({
-				["<leader>h"] = { "Git [H]unk" },
-			}, { mode = "v" })
 		end,
 	},
 	{ -- Fuzzy Finder (files, lsp, etc)
