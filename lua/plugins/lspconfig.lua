a08e154accff8200ccd6d479d077196f5d6d2a8e
tjazerzen <tjaz.erzen@zerodays.dev>
2025-02-13 09:52:15 +0100

    feat: add terraform ls

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -121,6 +121,7 @@ return { -- LSP Configuration & Plugins
 
 		-- LSP servers and clients are able to communicate to each other what features they support.
 		--  By default, Neovim doesn't support everything that is in the LSP specification.
+		--
 		local capabilities = vim.lsp.protocol.make_client_capabilities()
 		capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
 
@@ -213,6 +214,7 @@ return { -- LSP Configuration & Plugins
 					syntaxDocumentation = { enable = true },
 				},
 			},
+			terraformls = {},
 		}
 
 		local formatters = {
